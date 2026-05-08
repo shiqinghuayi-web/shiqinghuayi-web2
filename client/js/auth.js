7b5faf4e7522415d3e226ceff46035387399644b
shiqinghuayi-web <johnnygamedesu0209@gmail.com>
2026-05-08 11:21:59 +0800

    Update auth.js

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,5 +1,5 @@
 // 1. 確保網址是 https 且結尾沒有多餘斜線
-const API_BASE_URL = 'https://shiqingbackend.loca.lt';
+const API_BASE_URL = 'https://shiqingbackend30678.loca.lt';
 
 console.log('AUTH JS (Login) 已載入 - 支援 Admin 跳轉');
 
