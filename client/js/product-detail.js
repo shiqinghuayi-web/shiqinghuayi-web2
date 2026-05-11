012c015836a16caac9b5301fecb3644c6e59897a
shiqinghuayi-web <johnnygamedesu0209@gmail.com>
2026-05-11 11:57:56 +0800

    Update product-detail.js

--- client/js/product-detail.js
+++ client/js/product-detail.js
@@ -1,6 +1,5 @@
 // 請確保 config.js 在此檔案之前載入，或者手動定義 API_BASE_URL
-const API_BASE_URL = 'https://643df25ad9157656-39-12-34-105.serveousercontent.com'; 
-const FALLBACK_IMAGE = "https://images.unsplash.com/photo-1517705008128-361805f42e86?auto=format&fit=crop&w=1200&q=80";
+
 
 // 工具函式：支援後端上傳路徑
 const getProductImage = (p) => {
