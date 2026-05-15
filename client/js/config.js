76dde1922a933041d333e5cc324f308a58ae75d9
shiqinghuayi-web <johnnygamedesu0209@gmail.com>
2026-05-15 08:33:40 +0800

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,4 +1,4 @@
 // client/js/config.js
 // 這裡是你唯一的「總開關」，以後隧道網址變了，只要改這裡就好！
-const API_BASE_URL = 'https://166c549392df467d-42-79-0-114.serveousercontent.com'; 
+const API_BASE_URL = 'https://5b4376711a55abb6-223-136-92-133.serveousercontent.com'; 
 const FALLBACK_IMAGE = "https://images.unsplash.com/photo-1517705008128-361805f42e86?auto=format&fit=crop&w=1200&q=80";
